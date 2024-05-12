aa91cd12a5f6e9a9726784bf16bf0b8a49112d83
N3v1 <n3v1010@gmail.com>
2024-05-12 11:53:22 +0200

    (feat): Add LICENSE

--- Package.swift
+++ Package.swift
@@ -5,17 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "SCSIntegrationKit",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14),
+        .visionOS(.v1)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SCSIntegrationKit",
             targets: ["SCSIntegrationKit"]),
+        .library(name: "SCSIntegrationKitUI", targets: ["SCSIntegrationKitUI"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ScribbleLabApp/ScribbleCoreServices-SDK.git", branch: "main")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SCSIntegrationKit"),
+            name: "SCSIntegrationKit", dependencies: []),
+        .target(name: "SCSIntegrationKitUI",
+                dependencies: ["SCSIntegrationKit"]),
         .testTarget(
             name: "SCSIntegrationKitTests",
             dependencies: ["SCSIntegrationKit"]),
---
+++ Sources/SCSIntegrationKitUI/SCSIntegrationKitUI.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  
+//
+//  Created by Nevio Hirani on 12.05.24.
+//
+
+import Foundation
+import SCSIntegrationKit
